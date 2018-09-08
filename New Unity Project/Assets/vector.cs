740a2a81f08ced10e90ea1bbcd7224d33729794d
Your Name <you@example.com>
2018-09-07 21:31:08 -0500

    first commit

---
+++ New Unity Project/Assets/vector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vector : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 velocity = new Vector3(0.3f, 0f, 0f);
+    Vector3 gravity = new Vector3(-6f, -3f, 0f); 
+    float t = 0f;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //velocity += gravity;
+        //transform.position += velocity;
+
+        t += 0.01f; 
+        transform.position = Vector3.Lerp(new Vector3(0.3f, 0f, 0f), new Vector3(-6f, -3f, 0f), t);
+        
+	}
+}
---
+++ New Unity Project/Assets/waypoints.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoints : MonoBehaviour {
+
+
+    float t = 0;
+    int index = 0;
+    int direction = 1;
+    List<Vector3> points = new List<Vector3>();
+    List<int> indices = new List<int>();
+    int[] path = new int[4]; 
+
+    void Start () {
+        points.Add(GameObject.Find("p1").transform.position);
+        points.Add(GameObject.Find("p2").transform.position);
+        points.Add(GameObject.Find("p3").transform.position);
+        points.Add(GameObject.Find("p4").transform.position);
+        points.Add(GameObject.Find("p5").transform.position);
+        points.Add(GameObject.Find("p6").transform.position);
+
+        GeneratePath();
+        
+    }
+	
+	void Update () {
+        
+        t += 0.01f;
+        transform.position = Vector3.Lerp(points[path[index]], points[path[index+1]], t);
+        if(t > 1)
+        {
+            t = 0;
+            index++;
+        }
+        if(index == path.Length -1) 
+        {
+            GeneratePath();
+            index = 0;
+        }
+
+        
+	}
+
+    void GeneratePath() 
+    {
+
+        Array.Clear(path, 0, path.Length);  
+        for (int runs = 0; runs < 4; runs++) 
+        {
+            int rndNumber = UnityEngine.Random.Range(1, 6);
+         
+            if(rndNumber != path[0] && rndNumber != path[1] && rndNumber != path[2] && rndNumber != path[3])
+            {
+                path[runs] = rndNumber;
+            }
+        }
+        Debug.Log(path);
+    }
+}
